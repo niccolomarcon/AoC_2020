3b3633b698734331dde3744b7677703711bda836
Niccolò Marcon <marcon.niccolo@gmail.com>
2020-12-09 17:13:00 +0100

    day 09 - part 2

--- day_09/day_09.py
+++ day_09/day_09.py
@@ -1,6 +1,20 @@
 from itertools import combinations
 
 
+def contiguous_set(ns, invalid_n):
+    i, j, s = 0, 1, sum(ns[:2])
+    while i < len(ns) and j < len(ns):
+        if s < invalid_n:
+            j += 1
+            s += ns[j]
+        elif s > invalid_n:
+            s -= ns[i]
+            i += 1
+        else:
+            return ns[i:j+1]
+    raise Exception
+
+
 def find_first_invalid(ns, preamble_length=25):
     assert len(ns) >= preamble_length
     for i, n in enumerate(ns[preamble_length:]):
@@ -15,4 +29,5 @@ if __name__ == '__main__':
     with open('input.txt') as input_file:
         ns = list(map(int, input_file))
         invalid = find_first_invalid(ns)
-        print(invalid)
+        invalid_sum = contiguous_set(ns, invalid)
+        print(min(invalid_sum) + max(invalid_sum))
